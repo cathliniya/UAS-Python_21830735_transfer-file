7352e22de37a6ce432dd80c7571212f06952ab39
cathliniya <122766475+cathliniya@users.noreply.github.com>
2024-02-23 19:27:17 +0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,26 @@
+import socket
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("TRANSFER FILE by Kelompok PHF")
+print(ascii_banner)
+
+sock = socket.socket()
+
+port = 8800
+host = '192.168.56.1'
+
+sock.connect((host, port))
+sock.send('Terdapat pesan dari client'.encode())
+
+file = open('uas.txt', 'wb')
+
+line = sock.recv(1024)
+
+while(line):
+    file.write(line)
+    line = sock.recv(1024)
+
+print('File telah diterima.')
+
+file.close()
+sock.close()
